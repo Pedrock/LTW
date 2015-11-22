45c9782492289fe327346b61da22cdab0de40b82
PedroS11 <up201306095@fe.up.pt>
2015-11-22 03:22:53 +0000

    Mensagens de erro com php, css nao funciona no php nao sei pq, a verificar

--- register.php
+++ register.php
@@ -6,11 +6,28 @@
 		&& strlen($_POST['password'])>=8
 		&& isSet($_POST['fname']) && isSet($_POST['lname'])
 		&& registerUser($_POST['email'],$_POST['password'],$_POST['fname'],$_POST['lname'])) {
+
 		header("Location: ./?register=success");
 		die();
 	}
 	else
-	{		
+	{	
+		if(!preg_match("/^[A-z]+/", $_POST['fname']))
+			$fname=true;
+		if(!preg_match("/^[A-z]+/", $_POST['lname']))
+			$lname=true;
+		
+		if(!preg_match("/^\w+[@]\w+[.]\w+/", $_POST['email']))
+			$email=true;
+		else if(isset($_POST['email']) && userExists($_POST['email']))
+			$email2=true;
+		
+		if(!isSet($_POST['password']) || strlen($_POST['password'])<8)
+			$password = true;
+		else if(isSet($_POST['password2']) && $_POST['password'] != $_POST['password2'])
+			$password2 = true;
+
 		include('templates/login_page.php');
 	}
+	
 ?>
--- templates/login_page.php
+++ templates/login_page.php
@@ -1,15 +1,19 @@
 <?php
-include_once('core/common.php');
+require_once('core/common.php');
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title><?php echo $lang['SITE_NAME'] ?></title>
 	<script src="<?php echo $_GLOBALS['jquery'] ?>"></script>
 	<script type="text/javascript" src="js/login.js"></script>
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
+	
 	<div id="wrapper">
 		<div id="content" class="login" >
 			<div class="spacing"> </div>
@@ -26,33 +30,62 @@ include_once('core/common.php');
 					<h2> <?php echo $lang['REGISTER'] ?> </h2>
 					<input type="text" id="fname" name="fname" placeholder="<?php echo $lang['FIRST_NAME'] ?>"
 					value="<?php if(isSet($_POST['fname'])) echo $_POST['fname'] ?>">	
+					<div class="ss">
+						<?php if(isset($fname))echo $lang['FIRST_NAME_WRONG'] ?>
+					</div>
+
 					<div id="error-reg-fname" class= "error-reg">
 						<?php echo $lang['FIRST_NAME_WRONG'] ?>
 					</div>
 
+					
+
 					<input type="text" id="lname" name="lname" placeholder="<?php echo $lang['LAST_NAME'] ?>"
 					value="<?php if(isSet($_POST['lname'])) echo $_POST['lname'] ?>">
+					<div >
+						<?php if(isset($lname))echo $lang['LAST_NAME_WRONG'] ?>
+					</div>
 					<div id="error-reg-lname" class= "error-reg">
 						<?php echo $lang['LAST_NAME_WRONG'] ?>
 					</div>
 					
 					<input type="text" name="email" placeholder="<?php echo $lang['EMAIL'] ?>"
-					value="<?php if(isSet($_POST['email'])) echo $_POST['lname'] ?>">
+					value="<?php if(isSet($_POST['email'])) echo $_POST['email'] ?>">
+
+					<!-- Emails Usados-->
 					<div id="error-reg-email" class= "error-reg">
 						<?php echo $lang['EMAIL_USED'] ?>
 					</div>
+
+					<div >
+						<?php if(isset($email2))echo $lang['EMAIL_USED'] ?>
+					</div>
+					<!-- Emails Errados-->
+
 					<div id="error-reg-emailnV" class= "error-reg">
 						<?php echo $lang['EMAIL_WRONG'] ?>
+					</div>
+
+					<div >
+						<?php if(isset($email))echo $lang['EMAIL_WRONG'] ?>
 					</div>
 
 					<input type="password" name="password" placeholder="<?php echo $lang['PASSWORD'] ?>">
 					<div id="error-reg-pass" class= "error-reg">
 						<?php echo $lang['PASSWORD_LENGTH'] ?>
 					</div>
+
+					<div >
+						<?php if(isset($password))echo $lang['PASSWORD_LENGTH'] ?>
+					</div>
 					
 					<input type="password" name="password2" placeholder="<?php echo $lang['CONFIRM_PASSWORD'] ?>">
 					<div id="error-reg-pass2" class= "error-reg">
 						<?php echo $lang['PASSWORD_DIFF'] ?>
+					</div>
+
+					<div >
+						<?php if(isset($password2))echo $lang['PASSWORD_DIFF'] ?>
 					</div>
 
 					<input type="submit" class="button" value="<?php echo $lang['REGISTER'] ?>">
