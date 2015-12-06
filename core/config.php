6f4d2acee0c2a165070a5ff43fc136a1d2d6ea8d
Pedrock <camara-pedro@hotmail.com>
2015-12-06 16:19:27 +0000

    Agora funciona em qualquer pasta sem alterar o config.php excepto o 404 que é relativo à raiz do www

--- core/config.php
+++ core/config.php
@@ -1,5 +1,5 @@
 <?php
-  $_CONFIG['web_root'] = '/trabalho/';
+  $_CONFIG['web_root'] = str_replace("\\","",dirname($_SERVER['SCRIPT_NAME'])).'/';
   $_CONFIG['jquery'] = 'js/jquery-1.11.3.min.js';
   $_CONFIG['allowed_image_types'] = array(IMAGETYPE_GIF,IMAGETYPE_JPEG,IMAGETYPE_PNG);
   $_CONFIG['uploads_path'] = 'uploads/';
